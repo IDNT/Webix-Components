1775d3b9ad6b8638a6be99eb6bfb5c63a76b0b48
marc <marcus.zoller@idnt.net>
2016-04-11 19:10:11 +0200

    - New option "autoLabelPositioning" to switch between top and left label position depending on the available form width. Switching width is controled by option defaultLabelWidth. Changed upon resize.
    - Now adjusts bottomPadding automatically on available form width. Changed upon resize.

--- h5form/1.1.js
+++ h5form/1.1.js
@@ -4,9 +4,6 @@
  *
  * Provided under MIT License (MIT)
  * Copyright (C) 2016 IDNT Europe GmbH (www.idnt.net)
- *
- * v1.0 Marcus Zoller - Initial version using jQuery
- * v1.1 Marcus Zoller - Removed jQuery dependency
  */
  
 if (typeof webix === 'undefined') 
@@ -14,6 +11,12 @@ if (typeof webix === 'undefined')
 
 webix.protoUI({
 	name:'h5form',
+	defaults: {
+		autoLabelPositioning: true,
+		defaultLabelWidth: 130,
+		defaultLabelPosition: 'left',
+		autoheight:true,
+	},
 	$init:function(config) {
 		var oldobj = this._viewobj;	
 		this._contentobj = this._viewobj = webix.html.create("FORM",{
@@ -26,6 +29,8 @@ webix.protoUI({
 		
 		this.$view = this._viewobj;
 		
+		this.config = webix.extend(config, this.defaults, false);
+		
 		if (!this.$view.checkValidity)		
 			return; // We are done if we have no validation API
 		
@@ -33,7 +38,10 @@ webix.protoUI({
 		
 		config.elementsConfig = config.elementsConfig || {};
 		config.elementsConfig.on = config.elementsConfig.on || {};
-		
+		if (config.autoLabelPositioning) {
+			config.elementsConfig.labelPosition = config.elementsConfig.labelPosition || config.defaultLabelPosition;
+			config.elementsConfig.labelWidth = config.elementsConfig.labelWidth || config.defaultLabelWidth;
+		}
 		var savedOnChange = config.elementsConfig.on.onChange;
 		config.elementsConfig.on.onChange = function(newV, oldV) {
 			if (this.config.name) {
@@ -60,7 +68,7 @@ webix.protoUI({
 						valid = false;
 					}
 				}    
-			});
+			});			
 			return valid && (!savedObjRule !== 'function' || savedObjRule.call(this, arguments));
 		}
 		
@@ -79,4 +87,59 @@ webix.protoUI({
 			return false;
 		};
 	},
+	_initLabelPosition:function(adjust) {
+		var pos = 'left';
+		var width = this.getParentView().$width; 
+		if (width < 500)
+			pos='top';
+			
+		var chpos = chbp = false;
+		if (this.config.elementsConfig.labelPosition != pos) {
+			this.config.elementsConfig.labelPosition = pos;
+			chpos = true;
+		}
+		
+		for (var n in this.elements) {
+			var e = this.elements[n];
+			if (chpos) {
+				if (pos == 'top') {
+					e.config.labelPosition = 'top';
+					e._settings.labelPosition = 'top';
+					e._settings._savedLabelWidth = e._settings.labelWidth;
+					e._settings._savedHeight = e._settings.height;
+					e.config.height += 20;
+					//console.log(n+': saving labelWidth='+e._settings._savedLabelWidth+' height='+e._settings._savedHeight);
+					if (adjust)
+						e.render();
+				}
+				else if (pos == 'left') {
+					e.config.labelPosition = 'left';
+					e._settings.labelPosition = e.config.labelPosition;
+					e.config.labelWidth = e._settings._savedLabelWidth;
+					e._settings.labelWidth = e.config.labelWidth;
+					e.config.height = e._settings._savedHeight;
+				}
+			}
+			
+			// Adjust the bottom label if present
+			if (typeof e.config.bottomLabel !== 'undefined' && e.config.bottomLabel.length) {
+				var fWidth = e._input_width || (width - e.config.labelWidth);
+				var lSize = webix.html.getTextSize(e.config.bottomLabel);
+				var bp = Math.ceil(lSize.width / fWidth) * lSize.height;
+				if (e.config.bottomPadding != bp) {
+					e.config.bottomPadding = bp;
+					chbp = true;
+				}
+			}
+		}
+		
+		if (adjust && (chpos || chbp))
+			this.adjust();
+	},
+	$setSize:function(x,y){
+		var parentWidth = this.getParentView().$width;
+		if (typeof parentWidth !== 'undefined')
+			this._initLabelPosition(true);
+		return webix.ui.form.prototype.$setSize.call(this,x,y);
+	}
 }, webix.ui.form);
