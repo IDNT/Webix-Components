e76cab67459c83ae7548388b7e361192783b0ded
marc <marcus.zoller@idnt.net>
2016-04-17 23:07:38 +0200

    Fix for scrollY content

--- busyoverlay/1.1.js
+++ busyoverlay/1.1.js
@@ -8,6 +8,7 @@
  /*
  * Version History:
  * v1.1.0 IDNT/Marcus Zoller - Initial version
+ * v1.1.1 IDNT/Marcus Zoller - Fix for scrollY content
  */
 
 if (typeof webix === 'undefined') 
@@ -16,15 +17,22 @@ if (typeof webix === 'undefined')
 webix.idntBusyOverlay = {
 	showBusy:function(message){
 		if (!this._busyOverlay){
-			this._busyOverlay = webix.html.create("DIV",{ 'class':'webix_overlay', 'style':'position:fixed;display:block;height:100%;background-color:#fff;opacity:.9;color:#333;' },'');
+			// Save Y-Overflow style
+			this._busyOverflowY = this._viewobj.style.overflowY;
+			this._busyScrollTop = this._viewobj.scrollTop;
+			this._viewobj.scrollTop = 0;
+			this._viewobj.style.overflowY = 'hidden';
+			this._busyOverlay = webix.html.create("DIV",{ 'style':'position:absolute;top:0;bottom:0;left:0;width:100%;height:100%;background-color:#fff;opacity:1;color:#333;z-index:10;text-align:center;' },'');
+			this._viewobj.firstChild.style.position = 'relative';
 			webix.html.insertBefore(this._busyOverlay, this._viewobj.firstChild, this._viewobj);
-			this._viewobj.style.position = 'relative';
 		} 
-		this._busyOverlay.innerHTML = '<div style="position:absolute;display:block;top:25%;left:20%;margin-top:-50px;"><i class="fa fa-spinner fa-pulse fa-5x"></i><div class="idnt_busyoverlay_message" style="margin-top:25px;font-size:1.2em;">'+(message||'')+'</div></div>';
+		this._busyOverlay.innerHTML = '<i style="margin-top:30%" class="fa fa-spinner fa-pulse fa-5x"></i><div class="idnt_busyoverlay_message" style="margin-top:25px;font-size:1.2em;">'+(message||'')+'</div>';
 		return this;
 	},
 	hideBusy:function(){
 		if (this._busyOverlay){
+			this._viewobj.style.overflowY = this._busyOverflowY;
+			this._viewobj.scrollTop = this._busyScrollTop;
 			webix.html.remove(this._busyOverlay);
 			this._busyOverlay = null;
 		}
